34a4525531de5896c22311a218602b602dc5b35c
Dev Wulf <93016392+wulfsounds@users.noreply.github.com>
2022-04-15 13:43:58 -0500

    Revert "update splash, server, logos"

--- models/User.js
+++ models/User.js
@@ -1,14 +1,7 @@
-
-//Borrowed bp code from unit 14-MVC mini_project
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class User extends Model {}
 
 User.init(
   {
@@ -34,22 +27,11 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [6],
       },
     },
-    //TODO: Add section for music and cuisine preferences (may or may not get used)
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
