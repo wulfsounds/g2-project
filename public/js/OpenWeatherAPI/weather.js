22acc302527f486266c48cd166434ca98980f3eb
Devin Wulf <devinlwulf@gmail.com>
2022-04-16 21:50:44 -0500

    update dayCal js, add lobash, moment

--- public/js/OpenWeatherAPI/weather.js
+++ public/js/OpenWeatherAPI/weather.js
@@ -1,7 +1,10 @@
+var moment = require('moment'); // http://momentjs.com/docs/
+var _ = require('lodash'); // https://lodash.com/docs
+
 // Display Date
-let date = moment().format('L');
-$('time').text(date)
-console.log(moment().format('L'))
+let date = moment().format("L");
+$("time").text(date);
+console.log(moment().format("L"));
 
 // Display weather using OpenWeather API and geolocation
 // Weather Variables
@@ -12,70 +15,62 @@ let apiKey = "1f56ef55a0f3d4c3bb739f2d664d73f9";
 getLocation();
 
 // Gather Location data based on user's IP
-function getLocation(){
-    if (navigator.geolocation){
-        userLocation = navigator.geolocation.getCurrentPosition(getWeather);
-    } else {
-        $('span').html('HOWS THE WEATHER?');
-    }
+function getLocation() {
+	if (navigator.geolocation) {
+		userLocation = navigator.geolocation.getCurrentPosition(getWeather);
+	} else {
+		$("span").html("HOWS THE WEATHER?");
+	}
 }
 
-
 async function getWeather(position) {
-    let [lat, lon] = [position.coords.latitude, position.coords.longitude]
-    console.log(lat, lon);
-    let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+	let [lat, lon] = [position.coords.latitude, position.coords.longitude];
+	console.log(lat, lon);
+	let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-    await fetch(queryURL)
-        .then(function(response) {
-            console.log(response.statusText);
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            weather = data;
-            return weather;
-        })
-    postWeather();
+	await fetch(queryURL)
+		.then(function (response) {
+			console.log(response.statusText);
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			weather = data;
+			return weather;
+		});
+	postWeather();
 }
 
 // Displays weather on page with icons
 async function postWeather() {
-    console.log(weather);
-    let icon = weather.current.weather[0].icon;
-    let temp = weather.current.temp;
-    console.log(icon, temp);
-    // Displays weather icon
-    const weatherText = document.querySelector(".realTemp");
-    let img = document.createElement("img");
+	console.log(weather);
+	let icon = weather.current.weather[0].icon;
+	let temp = weather.current.temp;
+	console.log(icon, temp);
+	// Displays weather icon
+	const weatherText = document.querySelector(".realTemp");
+	let img = document.createElement("img");
 	img.setAttribute(`src`, `http://openweathermap.org/img/wn/${icon}@2x.png`);
 	img.setAttribute("alt", "weather-icon");
 	img.setAttribute("class", "icon");
-    weatherText.appendChild(img);
+	weatherText.appendChild(img);
 
+	// Displays temp (in Fº)
+	$(".local").text(`${Math.floor(temp)}º`);
 
-    // Displays temp (in Fº)
-    $('.local').text(`${Math.floor(temp)}º`)
-
-    fiveDay();
+	fiveDay();
 }
-
 
 async function fiveDay() {
 	console.log("fiveDay OK");
 	console.log(weather);
 	let i = 0;
-	const weatherStats = [weather.daily.slice(1, 6)];
+	const weatherObject = [weather.daily.slice(1, 6)];
+	console.log(`WeatherObject: ${weatherObject}`);
 
 	weather.daily.slice(1, 6).forEach(function (weatherObject) {
 		let icon = weather.daily[i].weather[0].icon;
 
-		let dayBox = document.createElement("div");
-		dayBox.setAttribute("class", "col-2 day-box");
-		let forecast = document.createElement("div");
-		forecast.setAttribute("class", "forecast");
-		let dayDate = document.createElement("p");
-		dayDate.setAttribute("class", "day-date");
 		let dayIcon = document.createElement("img");
 		dayIcon.setAttribute(
 			`src`,
@@ -83,24 +78,50 @@ async function fiveDay() {
 		);
 		let dayTemp = document.createElement("p");
 		dayTemp.setAttribute("class", "day-temp");
-		let dayWind = document.createElement("p");
-		dayWind.setAttribute("class", "day-wind");
-		let dayHumid = document.createElement("p");
-		dayHumid.setAttribute("class", "day-humid");
-		let dayUVI = document.createElement("p");
-		dayUVI.setAttribute("class", "day-uvi");
-		let dayBadge = document.createElement("span");
-		dayBadge.setAttribute("class", "day-badge");
 
-		$(".five-day").append(dayBox);
-		$(dayBox).append(forecast);
-		$(forecast).append(dayDate);
-		$(forecast).append(dayIcon);
-		$(forecast).append(dayTemp);
+		
+		console.log(dayIcon);
+		console.log(dayTemp);
+
+		$(".five-day").append(dayIcon);
+		$(".five-day").append(dayTemp);
 		$(dayTemp).text(`Temp: ${Math.floor(weather.daily[i].temp.day)}`);
 
 		i++;
 	});
+    let day;
+    let dayDates = document.createElement("p");
+    dayDates.setAttribute("class", "day-date"); 
+    addDate(day);
+    dayDates.text(day)
+    $(".five-day").append(dayDates);
 }
 
-
+function addDate(collectionsWithDateValues){
+  var slots = [];
+  var hours = {
+    start: 7,   // 7am
+    end: 21,    // 9pm
+    window: 2   // How long each item should be slotted for.
+  };
+  var rightNow  = moment().add(0, 'days').hours(hours.start).minute(0).second(0);
+  var cutoff    = moment(rightNow).add(5,'days'); // Check the next 2 weeks.
+  
+  for( rightNow ; rightNow.isBefore(cutoff) ; rightNow.add(hours.window, 'hours') ){
+    // Check if we're going beyond the daily cutoff, go to the next day
+    if(rightNow.isAfter(moment(rightNow).hour(hours.end))){
+      rightNow.add(1, 'days').hour(hours.start);
+    }
+  
+    var foundClash = false;
+    _.forEach(collectionsWithDateValues,  function(item){
+      // Check if the item is within now and the slotted time 
+      foundClash = moment(item.date).isBetween(rightNow, moment(rightNow).add(hours.window, 'hours').subtract(1, 'minutes').seconds(59));
+    });
+  
+    if(!foundClash){
+      slots.push(rightNow.toString());
+    }
+  }
+  return slots;
+}
