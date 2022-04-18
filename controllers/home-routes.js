4cf9eec347cd4730c299042bcdb38c2184307fc7
Devin Wulf <devinlwulf@gmail.com>
2022-04-18 11:09:48 -0500

    update home to show day

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,41 +5,34 @@ const { User, List, Day, Task, Category, Recipe, Wine, Playlist } = require('../
 // get daily view
 router.get('/day', async (req, res) => {
 
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-  } else {
+  try {
+    const userData = await User.findByPk(15, {
+      attributes: { exclude: ['password'] },
+      include: [
+        { model: Day,
+          where: {
+            day_time: ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', 
+            '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', 
+            '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', 
+            '7 PM', '8 PM', '9 PM', '10 PM', '11 PM']
+          },
+          include: [ {model: Task} ]
+        }
+      ],
+    });
+  
+    const user = userData.get({ plain: true });
+    console.log(user);
 
-      try {
-        const userData = await User.findByPk(req.session.user_id, {
-          attributes: { exclude: ['password'] },
-          include: [
-            { model: List,
-              where: {
-                list_date: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-              },
-              include: [ {model: Task} ]
-            }
-          ],
-        });
-      
-        const user = userData.get({ plain: true });
-        console.log(user);
-
-        // const newCulExp = await Category.findByPk(req.params.id, {
-        //   include: [ { model: Recipe, Wine, Playlist } ],
-        // });
-        // const culExp = newCulExp.get({ plain: true });
-
-        res.render('homepage', {
-          ...user, 
-          // ...culExp,
-          loggedIn: req.session.loggedIn,
-        });
-        
-      } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-      }
+    res.render('dayCal', {
+      ...user, 
+      // ...culExp,
+      loggedIn: req.session.loggedIn,
+    });
+    
+  } catch (err) {
+  console.log(err);
+  res.status(500).json(err);
   }
 
 });
